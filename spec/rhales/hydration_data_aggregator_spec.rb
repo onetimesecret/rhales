e56bfc07192f2363d455754498f6660507bdcc51
delano <delano@onetimesecret.com>
2025-07-10 09:54:18 -0700

    Remove frozen string comments

--- spec/rhales/hydration_data_aggregator_spec.rb
+++ spec/rhales/hydration_data_aggregator_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# spec/rhales/hydration_data_aggregator_spec.rb
 
 require 'spec_helper'
 
--- spec/rhales/rue_parser_improvements_spec.rb
+++ spec/rhales/rue_parser_improvements_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# spec/rhales/rue_parser_improvements_spec.rb
 
 require 'spec_helper'
 
